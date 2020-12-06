627b034dfec5eb063f22896c3763dd9e5fb54824
Sangdok Mo <sdmo@daum.net>
2020-12-06 18:31:55 +0900

    fix compare issue in avrdude-arduino.sh
    Signed-off-by: Sangdok Mo <sdmo@daum.net>

--- tools/avrdude-arduino.sh
+++ tools/avrdude-arduino.sh
@@ -10,15 +10,15 @@ if [ $# -ne 3 ]; then
   exit
 fi
 
-if [ $1 == "nano" ]; then
+if [ $1 = "nano" ]; then
   echo "uploading to $1 board..."
   avrdude -v -c arduino -p m328p -P $2 -b 115200 -Uflash:w:$3:i
 
-elif [ $1 == "uno" ]; then
+elif [ $1 = "uno" ]; then
   echo "uploading to $1 board..."
   avrdude -v -c arduino -p m328p -P $2 -b 115200 -Uflash:w:$3:i
 
-elif [ $1 == "mega" ]; then
+elif [ $1 = "mega" ]; then
   echo "uploading to $1 board..."
   avrdude -v -c wiring -p m2560 -P $2 -b 115200 -Uflash:w:$3:i
 
